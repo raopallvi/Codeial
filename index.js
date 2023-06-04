c5a302998bfee6f6b29ec9a5436e1b03002978c5
pallvi rao <raopallvi599@gmail.com>
2023-06-04 17:53:38 +0530

    Set view engine , views  & install ejs

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+const path = require('path');
+app.set('view engine' , 'ejs');
+app.set('views' , path.join(__dirname , "views"));
 /* Use express routers */
 app.use('/' , require('./routes'));
-
 app.listen(port , function(err)
 {
     if(err)

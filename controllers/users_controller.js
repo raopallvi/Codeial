34913965c21af280dd7e1314cf8ed65ffb24bf0d
pallvi rao <raopallvi599@gmail.com>
2023-07-02 18:31:07 +0530

    Just Done sign Up

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,19 +1,7 @@
 const mongoose = require("mongoose");
 const User = require('../models/user');
 module.exports.profile = function (req, res) {
-    if (req.cookies.user_id) {
-        User.findById(req.cookies.user_id)
-            .then(user => {
-                return res.render('userProfile', user);
-            })
-            .catch(err => {
-                return res.redirect('/users/signIn');
-            })
-    }
-    else
-    {
-        return res.redirect('/users/signIn');
-    }
+    return res.render('userProfile');
 }
 
 module.exports.post = function (req, res) {
@@ -62,35 +50,6 @@ module.exports.create = function (req, res) {
         })
 }
 
-// Sign in and create session for user
 module.exports.createSession = function (req, res) {
-    // Find user
-    User.findOne({ Email: req.body.Email })
-        .then(user => {
-            if (user) {
-                // Handle user found
-                if (user.Password != req.body.Password) {
-                    // Handle mismatching of passwords which don't match
-                    return res.redirect('back');
-                }
-                else {
-                    // Handle session creation
-                    res.cookie('user_id', user._id);
-                    return res.render('userProfile', user);
-                }
-            }
-            else {
-                //Handle user not found
-                return res.redirect('back');
-            }
-        })
-        .catch(err => {
-            return console.log("Error in finding user during sign in !", err);
-        })
-}
-
-module.exports.signOut = function(req , res)
-{
-    res.cookie('user_id', null);
-    return res.redirect('/users/signIn');
+    //To Do
 }

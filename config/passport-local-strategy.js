fe5270f8e40aa12dc474d422d53060c68d0ab129
pallvi rao <raopallvi599@gmail.com>
2023-07-02 20:48:05 +0530

    set up Passport config

---
+++ config/passport-local-strategy.js
@@ -0,0 +1,68 @@
+// The main Passport module.
+const passport = require('passport');
+// A strategy provided by Passport for authenticating using a username and password combination.
+const LocalStrategy = require('passport-local').Strategy;
+// requiring user model (or schema)
+const User = require('../models/user');
+
+// authentication using passport
+// creating an instance of LocalStrategy
+passport.use(new LocalStrategy({
+    // this is main authentication field which is our email
+    usernameField: 'Email',
+},
+    // this is a callback function
+    // done is a function according to result (sucess or error)
+    // when user tries to authentication then this function executes with required field for authentication
+    function (Email, Password, done) {
+        // done is callback function which is reporting to passport js
+        // find user and establish identity
+        User.findOne({ Email: Email })
+            .then(user => {
+                if (!user || user.Password != Password) {
+                    console.log("Invalid User Name and Password !");
+                    // null means no error and false means authentication not done
+                    return done(null, false);
+                }
+                else {
+                    return done(null, user);
+                }
+            })
+            .catch(err => {
+                console.log("Error in finding user : ", err);
+                return done(err);
+            })
+    }
+));
+
+// Serializing and deserializing 
+// putting user_id in cookie is serializing  and then using that user_id in 
+// server side to find user in database this process is called deserialzing
+
+// Serializing the user to decide which key is stored in cookie
+// In the serialization step, Passport determines what user data should be stored in the session:
+
+// The passport.serializeUser function is called to specify how the user object should be serialized.
+// In this case, it serializes the user by storing the user's _id property in the session.
+passport.serializeUser(function (user, done) {
+    done(null, user._id);
+});
+
+// In the deserialization step, Passport retrieves the user object from the session based on the stored information:
+
+// The passport.deserializeUser function is called to specify how to retrieve a user object from the serialized data.
+// In this case, it retrieves the user object by finding it in the database based on the stored _id.
+// deserializing the user from key in cookies
+passport.deserializeUser(function (id, done) {
+    User.findById(id)
+        .then(user => {
+            return done(null , user);
+        })
+        .catch(err => {
+            console.log("Error in finding user : ", err);
+            return done(err);
+        })
+});
+
+// exports so that we can use it in other parts to our application
+module.exports = passport;

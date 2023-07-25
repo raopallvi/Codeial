e301c8b0ee34c7feb9954093bf35051f1c51cb1d
pallvi rao <raopallvi599@gmail.com>
2023-07-25 09:13:38 +0530

    post config setup

--- models/post.js
+++ models/post.js
@@ -4,12 +4,15 @@ const postSchema = new mongoose.Schema({
     content: {
         type: string,
         required: true
-    },// Post is done by user so we have to attahc or referenvce this post with user
+    },// Post is done by user so we have to attach or referenvce this post with user
     user: {
         type : mongoose.Schema.Types.ObjectId ,// always unique
         ref : 'User'
     }
-})
+},{
+    timestamps:true
+    }
+)
 
 const post = mongoose.model('post', postSchema);
 module.exports = post;

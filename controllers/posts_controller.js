e93e8f881e998ea0d5391fd7851fad603818d676
pallvi rao <raopallvi599@gmail.com>
2023-08-01 09:10:41 +0530

    Post deleted

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 module.exports.create = function (req, res) {
     Post.create({
         content: req.body.content,
@@ -11,5 +12,40 @@ module.exports.create = function (req, res) {
     })
     .catch(err=>{
         console.log("Error in creating a post !" , err);
+    })
+}
+
+module.exports.destroy = function(req , res)
+{
+    console.log("id:" , req.params.id );
+    
+    Post.findById({_id : req.params.id})
+    .then(post=>{
+        // when we compare two id's then they must be string.
+        if(post.user == req.user.id){
+            Post.deleteOne({ _id: req.params.id })
+            .then(() => {
+                // Delete all comments associated with the post
+                Comment.deleteMany({ post: req.params.id })
+                    .then(() => {
+                        return res.redirect('back');
+                    })
+                    .catch(err => {
+                        console.log("Error in deleting comments: ", err);
+                        return res.redirect('back');
+                    });
+            })
+            .catch(err => {
+                console.log("Error in deleting post: ", err);
+                return res.redirect('back');
+            });
+        }
+        else {
+            return res.redirect('back');
+        }
+    })
+    .catch(err=>{
+        console.log("Error in finding post for deletion :" , err);
+        return;
     })
 }
